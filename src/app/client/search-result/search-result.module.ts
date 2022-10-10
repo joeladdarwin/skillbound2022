08913a0011c450d83105a05d5d4b8eace9e0a922
Monisha-S17 <moni17122000@gmail.com>
2022-10-10 17:33:23 +0530

    error clean

--- src/app/client/search-result/search-result.module.ts
+++ src/app/client/search-result/search-result.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { SearchResultRoutingModule } from './search-result-routing.module';
 import { SearchResultComponent } from './search-result.component';
 
 @NgModule({
@@ -10,7 +9,7 @@ import { SearchResultComponent } from './search-result.component';
   ],
   imports: [
     CommonModule,
-    SearchResultRoutingModule,
+    
     
   ]
 })
